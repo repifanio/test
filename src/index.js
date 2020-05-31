0d7e0941c68c87d967673af3caf633bfbbc40307
Rafael Epifanio <rafael.cepifanio@gmail.com>
2020-05-31 02:13:04 -0300

    Teste de lint

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  return res.json({ message: "Teste de branch" })
+  return res.json({ message: 'Teste de branch master com lint' })
 })
 
 app.listen(3333, () => console.log('Server running on port 3333'))
